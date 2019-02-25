df4ca696fce0d31c73bd11ee258f59c418efc1f5
Christie Lynam <christielynam@gmail.com>
2019-02-25 12:50:54 -0700

    Remove comments

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const cors = require('cors')
 
 app.set('port', 3001)
 
-// this will parse request.body to JavaScript
 app.use(express.json())
 
-// this will allow requests from any website
 app.use(cors())
 
 app.listen(app.get('port'), () => console.log(`Magic happens on http://localhost:${app.get('port')}`))
@@ -15,7 +13,7 @@ app.listen(app.get('port'), () => console.log(`Magic happens on http://localhost
 app.locals.title = 'Presidents and Assholes'
 app.locals.presidents = []
 app.locals.presidents = [
-  {number:1, president: 'George Washington', birth_year: 1732, death_year: 1799, took_office: '1789-04-30', left_office: '1797-03-04', party: 'No Party'},
+  {number: 1, president: 'George Washington', birth_year: 1732, death_year: 1799, took_office: '1789-04-30', left_office: '1797-03-04', party: 'No Party'},
   {number: 2, president: 'John Adams', birth_year: 1735, death_year: 1826,took_office: '1797-03-04', left_office: '1801-03-04', party: 'Federalist'},
   {number: 3, president: 'Thomas Jefferson', birth_year: 1743, death_year: 1826, took_office: '1801-03-04', left_office: '1809-03-04', party: 'Democratic-Republican'},
   {number: 4, president: 'James Madison', birth_year: 1751, death_year: 1836, took_office: '1809-03-04', left_office: '1817-03-04', party: 'Democratic-Republican'},
